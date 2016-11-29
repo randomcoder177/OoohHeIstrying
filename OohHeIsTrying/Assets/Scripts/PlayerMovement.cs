a597a0cb4911f6adb6ca3bbdcbdc62181104ba8d
Danny-PC\Danny <donie177@gmail.com>
2016-11-29 22:55:37 +0100

    PlayerMovement updated, removed vertical movement

--- OohHeIsTrying/Assets/Scripts/PlayerMovement.cs
+++ OohHeIsTrying/Assets/Scripts/PlayerMovement.cs
@@ -3,26 +3,44 @@ using System.Collections;
 
 [RequireComponent (typeof (Rigidbody))]
 public class PlayerMovement : MonoBehaviour {
-    public float playerSpeedHorizontal = 20f;
-    public float playerSpeedVertical = 10f;
+    public float playerSpeedHorizontal = 200f;
+    public float playerJumpStrength = 500f;
+
+    public float jumpDelay = 1f;
+    private bool canJump;
+
 
     private Rigidbody rb;
+    
 
 	// Use this for initialization
 	void Start () {
+        canJump = true;
         rb = GetComponent<Rigidbody>();
 	
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+        // Movement script
         float horizontalAxis = Input.GetAxis("Horizontal");
-        float verticalAxis = Input.GetAxis("Vertical");
-        Vector3 movementVector = new Vector3(horizontalAxis, verticalAxis, 0);
-        if (horizontalAxis != 0 || verticalAxis != 0)
+        Vector3 movementVector = new Vector3(horizontalAxis, 0, 0);
+        if (horizontalAxis != 0)
         {
-            rb.AddRelativeForce(movementVector);
+            rb.AddForce(movementVector);
+        }
+
+        if (Input.GetButton("Jump") && canJump == true)
+        {
+            rb.AddForce(Vector3.up * playerJumpStrength);
+            canJump = false;
+            Invoke("ResetJump", jumpDelay);
         }
 	
 	}
+
+    private void ResetJump()
+    {
+        canJump = true;
+    }
 }
