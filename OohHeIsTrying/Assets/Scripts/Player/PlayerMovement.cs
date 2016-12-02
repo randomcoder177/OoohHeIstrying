5ab5b55322fd731886c8d8804ba6e902b8c9bbef
Danny-PC\Danny <donie177@gmail.com>
2016-12-02 20:03:23 +0100

    PlayerMovement zatim hotovo

--- OohHeIsTrying/Assets/Scripts/Player/PlayerMovement.cs
+++ OohHeIsTrying/Assets/Scripts/Player/PlayerMovement.cs
@@ -5,6 +5,7 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour {
     public float playerSpeedHorizontal = 200f;
     public float playerJumpStrength = 500f;
+    public float playerSpeedLimit = 100f;
 
     public float jumpDelay = 1f;
     private bool canJump;
@@ -23,13 +24,12 @@ public class PlayerMovement : MonoBehaviour {
 	}
 	
 	void FixedUpdate () {
-        // Movement script
+        // Movement
         if (Input.GetAxis("Horizontal") != 0)
         {
             Vector3 movementVector = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
             rb.AddRelativeForce(movementVector * playerSpeedHorizontal);
         }
-
 
         // Jumping
         if (Input.GetButton("Jump") && canJump == true)
@@ -37,6 +37,12 @@ public class PlayerMovement : MonoBehaviour {
             rb.AddForce(Vector3.up * playerJumpStrength);
             canJump = false;
             Invoke("ResetJump", jumpDelay);
+        }
+        // Limit player speed - checks velocity magnitude vector and if it's over the speedLimit,
+        // increases drag
+        if (rb.velocity.magnitude > playerSpeedLimit)
+        {
+            rb.velocity = rb.velocity.normalized * playerSpeedLimit;
         }
 	
 	}
