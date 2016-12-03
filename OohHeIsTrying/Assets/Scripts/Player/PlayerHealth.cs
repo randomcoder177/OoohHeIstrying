db9f688ccaf63633ed64009938c8db49764a9103
Danny-PC\Danny <donie177@gmail.com>
2016-12-03 09:02:50 +0100

    no message

---
+++ OohHeIsTrying/Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+    public float playerHealth = 200f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    void OnCollisionEnter (Collision otherObject)
+    {
+    }
+
+    void IncreaseHealth(float boost)
+    {
+        playerHealth += boost;
+    }
+
+    void DecreaseHealth(float damage)
+    {
+        playerHealth -= damage;
+    }
+}
